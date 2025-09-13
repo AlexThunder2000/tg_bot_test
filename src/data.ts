68d118b2c85c0b423f0d656dbe5b7154fe4649fd
AlexThunder2000 <gdsasha2000@gmail.com>
2025-09-13 22:39:30 +0300

    add back exercises

--- src/data.ts
+++ src/data.ts
@@ -23,16 +23,35 @@ export const CATEGORIES: Category[] = [
     key: "back",
     title: "Спина",
     exercises: [
-      { title: "Тяга верхнього блоку", fileId: "AAA_BACK_FILE_ID_1" },
-      { title: "Гіперекстензія", fileId: "AAA_BACK_FILE_ID_2" },
+      { title: "Вертикальна тяга", fileId: "BAACAgIAAyEGAAS2J90iAAMDaMV_ZDy6VNxqeS2iFQn1ob9sY-wAAk6OAAKQ9ylKvHiT8-o8Pto2BA" },
+      { title: "Вертикальна тяга зворотній хватом", fileId: "BAACAgIAAyEGAAS2J90iAAMEaMV_ZxH-tQT_t75hKalRo_czuBsAAk-OAAKQ9ylK3O1b7vqfARY2BA" },
+      { title: "Вертикальна тяга вузькою рукояткою", fileId: "BAACAgIAAyEGAAS2J90iAAMFaMV_afdpOchh3qi30zFGxN1XgRQAAlCOAAKQ9ylK60CenpfYO9k2BA" },
+      { title: "Вертикальна тяга 1 рукою", fileId: "BAACAgIAAyEGAAS2J90iAAMGaMV_bLIvU3gpomuxyNCHMHWeY5MAAlGOAAKQ9ylKov6VRfFe_AE2BA" },
+      { title: "Підтягування", fileId: "BAACAgIAAyEGAAS2J90iAAMHaMV_bjk1q0yxP1cFuAnJCphhCRQAAlKOAAKQ9ylK4hC4q1dM_Gw2BA" },
+      { title: "Підтягування вузьким хватом", fileId: "BAACAgIAAyEGAAS2J90iAAMIaMV_cRTKqH7i2FX3j0qqrGJ29KoAAlSOAAKQ9ylKmtuAQJBo6Po2BA" },
+      { title: "Горизонтальна тяга вузькою рукояткою", fileId: "BAACAgIAAyEGAAS2J90iAAMJaMV_dePB21GRfzBShrSI4lt8ba4AAlWOAAKQ9ylKsknEf3X7yHA2BA" },
+      { title: "Горизонтальна тяга широкою рукояткою", fileId: "BAACAgIAAyEGAAS2J90iAAMLaMV_e_0nZK1ylMZxtHiixWbE7noAAlmOAAKQ9ylKPukw2Y2NFaA2BA" },
+      { title: "Горизонтальна тяга 1 рукою", fileId: "BAACAgIAAyEGAAS2J90iAAMKaMV_efSsJ9y3MgABoZwk4T7QAwo4AAJXjgACkPcpSqx9Z304zicoNgQ" },
+      { title: "Тяга в кросовері 1 рукою на лавці", fileId: "BAACAgIAAyEGAAS2J90iAAMNaMV_gEqJfHFbK6UwC4G0b7-grZEAAluOAAKQ9ylKIIrPURAGsdE2BA" },
+      { title: "Тяга 2 руками в кросовері сидячи(Ісус)", fileId: "BAACAgIAAyEGAAS2J90iAAMOaMV_g_eaSzHRdA7US8CMnxioBuAAAlyOAAKQ9ylKSuPbt0o4lY82BA" },
+      { title: "Пуловер прямою рукояткою", fileId: "BAACAgIAAyEGAAS2J90iAAMPaMV_hR7u110VmhuwUsPCYs4hdw8AAl2OAAKQ9ylK4scGjvciIW42BA" },
+      { title: "Пуловер з канатом", fileId: "BAACAgIAAyEGAAS2J90iAAMQaMV_iM_WlmawAxMuOxq6q1KqrwgAAl6OAAKQ9ylKSatficxS8wk2BA" },
+      { title: "Австралійські підтягування", fileId: "BAACAgIAAyEGAAS2J90iAAMRaMV_jZBNSxxoOEEv-xrqwVsnLngAAl-OAAKQ9ylKGcWO6mEOtq02BA" },
+      { title: "Тяга штанги до поясу зворотній хватом", fileId: "BAACAgIAAyEGAAS2J90iAAMSaMV_j1s0qevrq0xXs577EBWQw1oAAmCOAAKQ9ylKpzj02aReylw2BA" },
+      { title: "Тяга штанги до поясу прямим хватом", fileId: "BAACAgIAAyEGAAS2J90iAAMTaMV_khXUuorbl5kmR4pfnQMVToIAAmGOAAKQ9ylKJsdio8rzzgM2BA" },
+      { title: "Тяга канатом під груди в кросовері", fileId: "BAACAgIAAyEGAAS2J90iAAMUaMV_lVqCpBY4eit-RdFlXsi2wD0AAmOOAAKQ9ylK-TqwiQsNt_Q2BA" },
+      { title: "Тяга гантелі 1 рукою", fileId: "BAACAgIAAyEGAAS2J90iAAMXaMV_oRnbfkIDFohgzrTObrrJMbEAAmaOAAKQ9ylKzts6edTNSKA2BA" },
+
+      { title: "Тяга гантелей 2 руками упор в лавку", fileId: "BAACAgIAAyEGAAS2J90iAAMWaMV_nE5dyrKj3NxPGRxHH75eknsAAmWOAAKQ9ylK28bV6XFTyxo2BA" },
+      { title: "Т-тяга", fileId: "BAACAgIAAyEGAAS2J90iAAMYaMV_pEPiQfjPX6qB5RJglDXGXMoAAmeOAAKQ9ylKkPzg7ZpOaDs2BA" },
+   
     ],
   },
   {
     key: "chest",
     title: "Груди",
     exercises: [
-      { title: "Жим штанги лежачи", fileId: "AAA_CHEST_FILE_ID_1" },
-      { title: "Розведення гантелей", fileId: "AAA_CHEST_FILE_ID_2" },
+    
     ],
   },
   {
@@ -43,21 +62,21 @@ export const CATEGORIES: Category[] = [
         key: "front-delta",
         title: "Передня дельта",
         exercises: [
-          { title: "Підйом гантелей перед собою", fileId: "AAA_FRONT_DELTA_FILE_ID" },
+      
         ],
       },
       {
         key: "mid-delta",
         title: "Середня дельта",
         exercises: [
-          { title: "Жим штанги стоячи", fileId: "AAA_MID_DELTA_FILE_ID" },
+     
         ],
       },
       {
         key: "rear-delta",
         title: "Задня дельта",
         exercises: [
-          { title: "Тяга гантелей у нахилі", fileId: "AAA_REAR_DELTA_FILE_ID" },
+      
         ],
       },
     ],
@@ -66,24 +85,21 @@ export const CATEGORIES: Category[] = [
     key: "biceps",
     title: "Біцепс",
     exercises: [
-      { title: "Підйом штанги на біцепс", fileId: "AAA_BICEPS_FILE_ID_1" },
-      { title: "Підйом гантелей сидячи", fileId: "AAA_BICEPS_FILE_ID_2" },
+   
     ],
   },
   {
     key: "triceps",
     title: "Тріцепс",
     exercises: [
-      { title: "Французький жим", fileId: "AAA_TRICEPS_FILE_ID_1" },
-      { title: "Відтискання на брусах", fileId: "AAA_TRICEPS_FILE_ID_2" },
+   
     ],
   },
   {
     key: "glutes",
     title: "Сідниці",
     exercises: [
-      { title: "Ягодичний міст", fileId: "AAA_GLUTES_FILE_ID_1" },
-      { title: "Випади з гантелями", fileId: "AAA_GLUTES_FILE_ID_2" },
+    
     ],
   },
   {
@@ -94,21 +110,21 @@ export const CATEGORIES: Category[] = [
         key: "front-leg",
         title: "Передня поверхня",
         exercises: [
-          { title: "Присідання зі штангою", fileId: "AAA_FRONT_LEG_FILE_ID" },
+      
         ],
       },
       {
         key: "inner-leg",
         title: "Внутрішня поверхня",
         exercises: [
-          { title: "Зведення ніг у тренажері", fileId: "AAA_INNER_LEG_FILE_ID" },
+        
         ],
       },
       {
         key: "rear-leg",
         title: "Задня поверхня",
         exercises: [
-          { title: "Мертва тяга", fileId: "AAA_REAR_LEG_FILE_ID" },
+        
         ],
       },
     ],
